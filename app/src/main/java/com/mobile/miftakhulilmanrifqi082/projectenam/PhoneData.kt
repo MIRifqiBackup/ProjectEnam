831b76b206700ce88f4014622c4ac05a255eb881
Miftakhul-Ilman-Rifqi <bozzclax@gmail.com>
2023-07-31 23:55:17 +0700

    first commit

---
+++ app/src/main/java/com/mobile/miftakhulilmanrifqi082/projectenam/PhoneData.kt
@@ -0,0 +1,3 @@
+package com.mobile.miftakhulilmanrifqi082.projectenam
+
+data class PhoneData( val contactName: String, val phone: Long, val address: String, val email: String, val label: String, val apps: String )
